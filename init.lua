74d861821f0b8c5728814fd5a109fbe3849c9c3e
qcharlieshi <charlies@live.com>
2024-12-18 11:48:02 -0500

    first commit
    initial
    
    update
    
    update
    
    update

---
+++ init.lua
@@ -0,0 +1,3 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.g.python3_host_prog = "/Library/Frameworks/Python.framework/Versions/3.12/bin/python3"
---
+++ lua/config/autocmds.lua
@@ -0,0 +1,11 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds heredoc
+
+-- Allows modifiable quickfix windows
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "quickfix",
+  callback = function()
+    vim.opt_local.modifiable = true
+  end,
+})
