a059239be14f18f8afa23938c4d8fedfd42d955a
qcharlieshi <charlies@live.com>
2024-12-18 22:28:12 -0500

    update

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,7 +1,7 @@
 return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
+  lazy = false,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
@@ -20,12 +20,8 @@ return {
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/vaults/personal",
-      },
-      {
         name = "work",
-        path = "~/vaults/work",
+        path = "/Users/qcharlieshi/Library/Mobile Documents/iCloud~md~obsidian/Documents/Default/",
       },
     },
 
--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -4,6 +4,97 @@ return {
     local wilder = require("wilder")
     wilder.setup({ modes = { ":", "/", "?" } })
 
+    -- wilder.set_option("pipeline", {
+    --   wilder.branch(
+    --     wilder.python_file_finder_pipeline({
+    --       file_command = function(ctx, arg)
+    --         if string.find(arg, ".") ~= nil then
+    --           return { "fdfind", "-tf", "-H" }
+    --         else
+    --           return { "fdfind", "-tf" }
+    --         end
+    --       end,
+    --       dir_command = { "fd", "-td" },
+    --       filters = { "cpsm_filter" },
+    --     }),
+    --     wilder.substitute_pipeline({
+    --       pipeline = wilder.python_search_pipeline({
+    --         skip_cmdtype_check = 1,
+    --         pattern = wilder.python_fuzzy_pattern({
+    --           start_at_boundary = 0,
+    --         }),
+    --       }),
+    --     }),
+    --     wilder.cmdline_pipeline({
+    --       fuzzy = 2,
+    --       fuzzy_filter = wilder.lua_fzy_filter(),
+    --     }),
+    --     {
+    --       wilder.check(function(ctx, x)
+    --         return x == ""
+    --       end),
+    --       wilder.history(),
+    --     },
+    --     wilder.python_search_pipeline({
+    --       pattern = wilder.python_fuzzy_pattern({
+    --         start_at_boundary = 0,
+    --       }),
+    --     })
+    --   ),
+    -- })
+    --
+    -- local highlighters = {
+    --   wilder.pcre2_highlighter(),
+    --   wilder.lua_fzy_highlighter(),
+    -- }
+    --
+    -- local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+    --   border = "rounded",
+    --   empty_message = wilder.popupmenu_empty_message_with_spinner(),
+    --   highlighter = highlighters,
+    --   left = {
+    --     " ",
+    --     wilder.popupmenu_devicons(),
+    --     wilder.popupmenu_buffer_flags({
+    --       flags = " a + ",
+    --       icons = { ["+"] = "", a = "", h = "" },
+    --     }),
+    --   },
+    --   right = {
+    --     " ",
+    --     wilder.popupmenu_scrollbar(),
+    --   },
+    -- }))
+    --
+    -- local wildmenu_renderer = wilder.wildmenu_renderer({
+    --   highlighter = highlighters,
+    --   separator = " · ",
+    --   left = { " ", wilder.wildmenu_spinner(), " " },
+    --   right = { " ", wilder.wildmenu_index() },
+    -- })
+    --
+    -- wilder.set_option(
+    --   "renderer",
+    --   wilder.renderer_mux({
+    --     [":"] = popupmenu_renderer,
+    --     ["/"] = wildmenu_renderer,
+    --     substitute = wildmenu_renderer,
+    --   })
+    -- )
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
+    --
     wilder.set_option(
       "renderer",
       wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
@@ -15,5 +106,39 @@ return {
         border = "rounded",
       }))
     )
+
+    wilder.set_option("pipeline", {
+      wilder.branch(
+        wilder.python_file_finder_pipeline({
+          -- to use ripgrep : {'rg', '--files'}
+          -- to use fd      : {'fd', '-tf'}
+          file_command = { "rg", "--files" },
+          -- -- { "find", ".", "-type", "f", "-printf", "%P\n" },
+          -- to use fd      : {'fd', '-td'}
+          dir_command = { "fd", "-td" },
+          -- { "find", ".", "-type", "d", "-printf", "%P\n" },
+          -- -- use {'cpsm_filter'} for performance, requires cpsm vim plugin
+          -- found at https://github.com/nixprime/cpsm
+          filters = { "fuzzy_filter", "difflib_sorter" },
+        }),
+        wilder.cmdline_pipeline({
+          -- sets the language to use, 'vim' and 'python' are supported
+          language = "python",
+          -- -- 0 turns off fuzzy matching
+          -- 1 turns on fuzzy matching
+          -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+          fuzzy = 1,
+        }),
+        wilder.python_search_pipeline({
+          -- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+          -- pattern = wilder.python_fuzzy_pattern(),
+          -- omit to get results in the order they appear in the buffer
+          -- sorter = wilder.python_difflib_sorter(),
+          -- can be set to 're2' for performance, requires pyre2 to be installed
+          -- see :h wilder#python_search() for more details
+          --   engine = "re",
+        })
+      ),
+    })
   end,
 }
