0cbaae539331d45e42dd42b0745a6ab0033caf8d
qcharlieshi <charlies@live.com>
2025-07-22 16:50:59 -0400

    chore: move common colors out

---
+++ lua/config/colors.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.colors = {
+  light_blue = "#0ab9d7",
+  blue = "#80a0ff",
+  cyan = "#79dac8",
+  cyan_dark = "#4a9b8e",
+  black = "#080808",
+  white = "#c6c6c6",
+  red = "#ff5189",
+  violet = "#d183e8",
+  grey = "#303030",
+  orange = "#ff9500",
+  purple = "#1e2030",
+}
+
+return M
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,4 @@
-local colors = {
-  blue = "#80a0ff",
-  cyan = "#79dac8",
-  cyan_dark = "#4a9b8e",
-  black = "#080808",
-  white = "#c6c6c6",
-  red = "#ff5189",
-  violet = "#d183e8",
-  grey = "#303030",
-  orange = "#ff9500",
-  purple = "#1e2030",
-}
+local colors = require("config.colors").colors
 
 return {
   "nvim-lualine/lualine.nvim",
@@ -41,6 +30,7 @@ return {
           "snacks_picker",
           "claude",
           "terminal",
+          "edgy",
         },
       },
 
