9c913cc51cd502861dc018ef4df86133c68a1e87
qcharlieshi <charlies@live.com>
2025-08-06 15:33:39 -0400

    feat: add dashboard triforce animation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,84 @@
+local dbAnim = require("dashboardAnimation")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "snacks_dashboard",
+  callback = function()
+    -- Reset animation state when dashboard opens
+    dbAnim.shouldPlayAnimation = true
+    dbAnim.asciiImg = dbAnim.frames[1]
+  end,
+})
+
 return {
   {
     "folke/snacks.nvim",
+    init = function()
+      vim.defer_fn(function()
+        dbAnim.theAnimation(dbAnim.theAnimation)
+      end, 100)
+    end,
     opts = {
+      dashboard = {
+        on_close = function()
+          dbAnim.shouldPlayAnimation = false
+        end,
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[]],
+        },
+        sections = {
+          {
+            section = "header",
+            padding = 1,
+            function()
+              return { header = dbAnim.asciiImg }
+            end,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+        keys = {
+          {
+            icon = " ",
+            key = "f",
+            desc = "Find File",
+            action = function()
+              Snacks.dashboard.pick("files")
+              dbAnim.shouldPlayAnimation = false
+            end,
+          },
+          {
+            icon = " ",
+            key = "n",
+            desc = "New File",
+            action = function()
+              vim.cmd("ene | startinsert")
+              dbAnim.shouldPlayAnimation = false
+            end,
+          },
+          {
+            icon = " ",
+            key = "g",
+            desc = "Find Text",
+            action = function()
+              Snacks.dashboard.pick("live_grep")
+              dbAnim.shouldPlayAnimation = false
+            end,
+          },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          {
+            icon = " ",
+            key = "c",
+            desc = "Config",
+            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+          },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+      },
       picker = {
         layout = { -- the layout config
           fullscreen = true,
@@ -24,6 +101,7 @@ return {
           finder = "rg",
           supports_args = true,
           live = true,
+          -- TODO: figure out globs
           args = {
             "--hidden",
             "--follow",
@@ -31,6 +109,7 @@ return {
             "--glob=!node_modules",
             "--glob=!*.lock",
             "--multiline",
+            "--glob",
           },
           rg_glob = true,
         },
