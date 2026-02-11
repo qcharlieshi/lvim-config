10a051e869e3086ea3d8fd19127624fe1d7797da
qcharlieshi <charlies@live.com>
2026-02-11 14:22:48 -0500

    fix: codediff

--- lua/plugins/codediff.lua
+++ lua/plugins/codediff.lua
@@ -2,11 +2,4 @@ return {
   "esmuellert/codediff.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
   cmd = "CodeDiff",
-  config = function()
-    require("codediff").setup({
-      diff = {
-        disable_inlay_hints = true, -- Cleaner diff view
-      },
-    })
-  end,
 }
