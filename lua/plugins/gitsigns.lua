bd73ad4d37b6b2fb08e19ce5feb5b39368e402c8
qcharlieshi <charlies@live.com>
2026-04-10 09:18:15 -0400

    fix: gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,7 +1,4 @@
 return {
-  -- Disable mini.diff (LazyVim default) in favor of gitsigns
-  { "nvim-mini/mini.diff", enabled = false },
-
   {
     "lewis6991/gitsigns.nvim",
     event = "LazyFile",
@@ -17,14 +14,62 @@ return {
       show_deleted = true,
     },
     keys = {
-      { "]h", function() require("gitsigns").nav_hunk("next") end, desc = "Next Hunk" },
-      { "[h", function() require("gitsigns").nav_hunk("prev") end, desc = "Prev Hunk" },
-      { "<leader>ghd", function() require("gitsigns").toggle_deleted() end, desc = "Toggle Deleted Lines" },
-      { "<leader>ghp", function() require("gitsigns").preview_hunk() end, desc = "Preview Hunk" },
-      { "<leader>ghr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk" },
-      { "<leader>ghs", function() require("gitsigns").stage_hunk() end, desc = "Stage Hunk" },
-      { "<leader>ghu", function() require("gitsigns").undo_stage_hunk() end, desc = "Undo Stage Hunk" },
-      { "<leader>ghb", function() require("gitsigns").blame_line({ full = true }) end, desc = "Blame Line" },
+      {
+        "]h",
+        function()
+          require("gitsigns").nav_hunk("next")
+        end,
+        desc = "Next Hunk",
+      },
+      {
+        "[h",
+        function()
+          require("gitsigns").nav_hunk("prev")
+        end,
+        desc = "Prev Hunk",
+      },
+      {
+        "<leader>ghd",
+        function()
+          require("gitsigns").toggle_deleted()
+        end,
+        desc = "Toggle Deleted Lines",
+      },
+      {
+        "<leader>ghp",
+        function()
+          require("gitsigns").preview_hunk()
+        end,
+        desc = "Preview Hunk",
+      },
+      {
+        "<leader>ghr",
+        function()
+          require("gitsigns").reset_hunk()
+        end,
+        desc = "Reset Hunk",
+      },
+      {
+        "<leader>ghs",
+        function()
+          require("gitsigns").stage_hunk()
+        end,
+        desc = "Stage Hunk",
+      },
+      {
+        "<leader>ghu",
+        function()
+          require("gitsigns").undo_stage_hunk()
+        end,
+        desc = "Undo Stage Hunk",
+      },
+      {
+        "<leader>ghb",
+        function()
+          require("gitsigns").blame_line({ full = true })
+        end,
+        desc = "Blame Line",
+      },
     },
   },
 }
